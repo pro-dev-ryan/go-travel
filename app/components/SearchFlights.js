cb695fe402d42a0ee84dcc194a64d0afb9206102
pro-dev-ryan <azizul.developer@gmail.com>
2023-03-19 03:47:39 +0600

    search tab revise done

--- app/components/SearchFlights.js
+++ app/components/SearchFlights.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 const SearchFlights = () => {
   return (
-    <div className="mx-4 md:mx-16 font-headline text-secondary">
+    <div className="mx-4 md:mx-16 font-textline text-secondary">
       <form action="" className="space-y-4">
         <div className="flex flex-col gap-5 md:gap-2 md:flex-row ">
           {/* left-side */}
-          <div className="flex md:gap-2 md:w-1/2">
-            <div className="frame w-full md:w-2/3 text-medium text-center">
+          <div className="flex md:gap-2 md:w-[46%]">
+            <div className="frame w-full md:w-2/3 text-center">
               <Image
                 className="absolute right-0 top-1/3 mr-4"
                 src="../../assets/misc/2way.svg"
@@ -15,18 +15,16 @@ const SearchFlights = () => {
                 height={20}
                 alt="icon"
               />
-              <p className="searchDes left-1/2 md:left-0 -translate-x-1/2 md:translate-x-2 -top-4 font-semibold">
+              <p className="searchDes left-1/2 md:left-0 -translate-x-1/2 md:translate-x-2 -top-4">
                 From - To
               </p>
-              <input
-                type="text"
-                placeholder="set course"
-                className="w-full text-center md:text-left px-2 outline-none "
-              />
+              <p className="searchProp">
+                <span>Chennai</span> - <span>Thailand</span>
+              </p>
             </div>
             <div className="hidden md:inline-block frame px-2">
               <p className="searchDes">Trip</p>
-              <select name="" id="">
+              <select name="" id="" className="searchProp">
                 <option value="">Return</option>
                 <option value="">One-Way</option>
               </select>
@@ -36,22 +34,27 @@ const SearchFlights = () => {
           <div className="flex flex-1 md:w-1/2 space-x-5 md:space-x-0 justify-between md:justify-evenly">
             <div className="md:hidden frame px-2">
               <p className="searchDes">Trip</p>
-              <select name="" id="">
+              <select name="" id="" className="searchProp">
                 <option value="">Return</option>
                 <option value="">One-Way</option>
               </select>
             </div>
             <div className="frame p-1 md:px-[2%] flex-grow  text-center">
-              <p className="searchDes left-1/3 md:left-0  -translate-x-1/4 md:translate-x-0">
+              <p className="searchDes left-1/3 md:left-0  -translate-x-1/4 md:translate-x-2">
                 Depart-Return
               </p>
-              <input type="date" />
+              <p className="searchProp">
+                <span>13 Jan 23</span> - <span>16 Jan 23</span>
+              </p>
             </div>
           </div>
           {/* last field tab */}
           <div className="frame p-1 md:px-[2%] flex-1 hidden md:block">
             <p className="searchDes px-[1%]">Passengers-Class</p>
-            <input type="text" />
+            <p className="searchProp">
+              {" "}
+              <span>2 passenger </span>,<span>business</span>{" "}
+            </p>
           </div>
         </div>
         {/* Action  */}
