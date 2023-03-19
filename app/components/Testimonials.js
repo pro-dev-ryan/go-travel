6ddaf5673b38f99a3491f97034709e002098d506
pro-dev-ryan <azizul.developer@gmail.com>
2023-03-19 20:49:19 +0600

    testimonial tab done

--- app/components/Testimonials.js
+++ app/components/Testimonials.js
@@ -3,10 +3,10 @@ import Testimonial from "./Testimonial";
 const Testimonials = () => {
   return (
     <div className="cardFrame">
-      <h3 className="mb-20">
+      <h3 className="mb-20 md:mb-5">
         What Our Clients Says <br /> About Us
       </h3>
-      <div className="space-y-20">
+      <div className="space-y-20 md:space-y-0 mx-4 md:mx-16 stack-sect">
         {[...Array(3)].map((card, i) => (
           <Testimonial key={i} card={card} />
         ))}
