e5ccfaa9c00e5ce2ad1a3c36e47c4cdad4d03e9e
pro-dev-ryan <azizul.developer@gmail.com>
2023-03-22 01:57:13 +0600

    tab footer done

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,30 +1,61 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import footerLogo from "../../public/footerLogo.png";
 const Footer = () => {
   return (
-    <div className="footer-bg space-y-4">
-      <div className="flex flex-col items-center cardPlate ">
+    <div className="footer-bg space-y-4 md:space-y-3">
+      <div className="flex flex-col md:gap-5 md:mx-16 md:flex-row items-center md:items-start space-y-5 md:space-y-0 font-headline">
+        {/* Profile */}
+        <div className="hidden md:flex flex-col flex-1">
+          <Image src={footerLogo} alt="logo" width={60} />
+          <p className="text-light lightText w-1/2">
+            Lorem Ipsum is simply dummy text of the printing and type setting
+            industry.
+          </p>
+        </div>
         {/* Company */}
-        <div className="selection">
+        <div className="hidden md:block space-y-3">
+          <p className="text-light text-base font-textline">Company</p>
+          <ul className="flex flex-col gap-2 text-light lightText text-left">
+            <Link href="/">Events</Link>
+            <Link href="/">Blogs</Link>
+            <Link href="/">FAQ</Link>
+            <Link href="/">Join US</Link>
+          </ul>
+        </div>
+        <div className="selection md:hidden">
           <select className="select">
             <option value="">Company</option>
           </select>
         </div>
         {/* About */}
-        <div className="selection">
+        <div className="hidden md:block space-y-3">
+          <p className="text-light text-base font-textline">About</p>
+          <ul className="flex flex-col gap-2 text-light lightText text-left">
+            <Link href="/">Project</Link>
+            <Link href="/">Mission</Link>
+            <Link href="/">Services</Link>
+            <Link href="/">Our Story</Link>
+          </ul>
+        </div>
+        <div className="selection md:hidden">
           <select className="select">
             <option value="">About</option>
           </select>
         </div>
         {/* Contact Us */}
-        <div className="selection">
+        <div className="hidden md:block text-right">
+          <p className="text-light text-base font-textline mb-3">Contact Us</p>
+          <p className="text-light lightText">traveling@gol.com</p>
+          <p className="text-light lightText">123/CA, Las Vegas</p>
+        </div>
+        <div className="selection md:hidden">
           <select className="select">
             <option value="">Contact Us</option>
           </select>
         </div>
       </div>
-      <div className="py-2 center gap-3">
+      <div className="py-2 center md:mx-16 md:justify-start md:text-left gap-3">
         {/* Social Links */}
         <Link className="links" href="/">
           <Image src="/assets/footer/fb.svg" alt="" width={25} height={25} />
--- app/components/GetApp.js
+++ app/components/GetApp.js
@@ -1,14 +1,33 @@
 import Image from "next/image";
+import mobile from "../../public/assets/misc/getApp.png";
 
 const GetApp = () => {
   return (
-    <div className="getApp">
-      <div className="cardPlate relative -mb-14 flex justify-between items-center py-4">
-        <div className="space-y-3">
-          <h3 className="text-light">Download Our App</h3>
-          <p className="text-light">The best travel in the world</p>
+    <div className="getApp mx-4 md:mx-16">
+      <div className="relative -mb-14 flex justify-between items-center p-4 md:p-5">
+        <div className="">
+          <div className="space-y-3 md:space-y-0">
+            <h3 className="text-light">Download Our App</h3>
+            <p className="text-light">The best travel in the world</p>
+          </div>
+          <div className="border-2 hidden md:mt-3 md:flex border-white rounded bg-primary items-center my-auto">
+            <div>
+              <Image
+                src="/assets/misc/play-store.svg"
+                alt="Play-store"
+                width={45}
+                height={45}
+              />
+            </div>
+            <div className="mr-2">
+              <p className="uppercase text-light text-xs font-medium">
+                get it it on
+              </p>
+              <p className="font-extralight text-light">Google Play</p>
+            </div>
+          </div>
         </div>
-        <div className="border-2 border-white flex rounded bg-primary items-center my-auto">
+        <div className="border-2 md:hidden border-white flex rounded bg-primary items-center my-auto">
           <div>
             <Image
               src="/assets/misc/play-store.svg"
@@ -23,6 +42,9 @@ const GetApp = () => {
             </p>
             <p className="font-extralight text-light">Google Play</p>
           </div>
+        </div>
+        <div className="hidden md:block absolute right-5 bottom-5">
+          <Image src={mobile} alt={mobile} width={140} />
         </div>
       </div>
     </div>
