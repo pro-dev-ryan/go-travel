358e53ef1d503448faf5a159b3f29e91cb786a5e
pro-dev-ryan <azizul.developer@gmail.com>
2023-03-26 02:07:03 +0600

    nav for lg screen done

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { BiChevronDown } from "react-icons/bi";
+import logo from "/public/logo.png";
 import { open } from "../layout";
 import NavMenu from "./Clients/NavMenu";
 
@@ -15,7 +16,7 @@ export const links = [
 
 const menu = links?.map((link, i) => (
   <Link
-    className="text-sm border border-primary w-full text-center md:text-base font-medium xl:text-lg lg:ml-1 hover:text-primary md:px-2 xl:px-4 py-5 my-px"
+    className="text-sm border border-primary lg:border-none w-full lg:max-w-max text-center md:text-base hover:text-primary"
     key={i}
     href={link}
   >
@@ -37,25 +38,21 @@ const Navbar = () => {
         <span className="block md:hidden">
           <NavMenu />
         </span>
-        <div className="w-fit">
-          <Link href={"/"}>
-            <Image
-              className=""
-              src="/logo.png"
-              alt="logo"
-              width={60}
-              height={60}
-            />
-          </Link>
-          <div className="hidden lg:flex">{menu}</div>
+        <div className="flex w-full items-center gap-3">
+          <div className="w-14 lg:w-20">
+            <Link className="w-fit" href={"/"}>
+              <Image src={logo} alt="logo" width={150} />
+            </Link>
+          </div>
+          <div className="hidden lg:flex items-center lg:gap-4">{menu}</div>
         </div>
-        <div className="md:flex items-center gap-3 my-auto">
+        <div className="md:flex items-center gap-3 lg:gap-0 xl:my-auto">
           {sign?.map((signBtn) => (
             <button
               key={signBtn}
-              className={` lg:px-6 py-2 md:pt-1 md:text-xl font-semibold text-accent transition-colors duration-200 hover:text-neutral ${
+              className={` lg:px-4 py-2 md:pt-1 md:text-xl lg: text-base font-semibold text-accent transition-colors duration-200 hover:text-neutral ${
                 signBtn === "Sign Up" &&
-                "rounded-full text-light px-4 hover:text-white hidden lg:inline-block hover:bg-primary bg-accent"
+                "rounded-full text-light px-4 lg:px-6 lg:whitespace-nowrap hover:text-white hidden lg:inline-block hover:bg-primary bg-accent"
               } ${open.className} font-linkline`}
             >
               {signBtn}
